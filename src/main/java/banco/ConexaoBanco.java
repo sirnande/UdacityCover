b14e137d58f290479f299b770c7e60aad508ec7b
sirnande <sirnande.lima@hotmail.com>
2017-06-28 09:09:09 -0300

    Atualizado classe do banco

--- src/main/java/banco/ConexaoBanco.java
+++ src/main/java/banco/ConexaoBanco.java
@@ -11,21 +11,17 @@ public class ConexaoBanco {
 
     private static ConexaoBanco con;
     private static Connection conexao = null;
+    private static String dbUrl = "";
 
    private ConexaoBanco() {
        try{
 
                //Class.forName("com.mysql.jdbc.Driver");
-               conexao = DriverManager.getConnection("postgres://fzvfminzvijebo:07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77@ec2-23-23-86-179.compute-1.amazonaws.com:5432/db0c0krsvl11do", "fzvfminzvijebo", "07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77");
+               dbUrl = "jdbc:postgres://fzvfminzvijebo:07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77@ec2-23-23-86-179.compute-1.amazonaws.com:5432/db0c0krsvl11do?user=fzvfminzvijebo&password=07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77&sslmode=require";
+               conexao = DriverManager.getConnection(dbUrl);
 
-       }catch(ClassNotFoundException ex){
-           ex.printStackTrace();
-       }
-       catch(SQLException ex){
+       }catch(SQLException ex){
 
-           ex.printStackTrace();
-       }
-       catch(NoClassDefFoundError ex){
            ex.printStackTrace();
        }catch (Exception ex){
            ex.printStackTrace();
