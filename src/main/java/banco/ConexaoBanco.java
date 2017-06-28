ac695507e0334966aa2c98e2bcc06f39d708375d
sirnande <sirnande.lima@hotmail.com>
2017-06-28 10:03:10 -0300

    segunda atualizacao conexao banco

--- src/main/java/banco/ConexaoBanco.java
+++ src/main/java/banco/ConexaoBanco.java
@@ -20,9 +20,12 @@ public class ConexaoBanco {
                dbUrl = "jdbc:postgres://fzvfminzvijebo:07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77@ec2-23-23-86-179.compute-1.amazonaws.com:5432/db0c0krsvl11do?user=fzvfminzvijebo&password=07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77&sslmode=require";
                conexao = DriverManager.getConnection(dbUrl);
 
-       }catch(SQLException ex){
+               System.out.println("Fez requisicao");
 
+       }catch(SQLException ex){
+           System.out.println("Entro sql exception");
            ex.printStackTrace();
+
        }catch (Exception ex){
            ex.printStackTrace();
        }
