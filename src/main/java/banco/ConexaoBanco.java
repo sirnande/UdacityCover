b3cc16de962322f269e1f11377a5153a12cf5ca0
sirnande <sirnande.lima@hotmail.com>
2017-06-28 01:59:53 -0300

    Atualizadoo classe banco

--- src/main/java/banco/ConexaoBanco.java
+++ src/main/java/banco/ConexaoBanco.java
@@ -15,8 +15,8 @@ public class ConexaoBanco {
    private ConexaoBanco() {
        try{
 
-               Class.forName("com.mysql.jdbc.Driver");
-               conexao = DriverManager.getConnection("jdbc:mysql://localhost/udacity", "root", "utfpr");
+               //Class.forName("com.mysql.jdbc.Driver");
+               conexao = DriverManager.getConnection("postgres://fzvfminzvijebo:07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77@ec2-23-23-86-179.compute-1.amazonaws.com:5432/db0c0krsvl11do", "fzvfminzvijebo", "07c42fb05b10eccfd0f0378e48e74c8bd8a5e5bcf6a571fe4a2c5830a45e9c77");
 
        }catch(ClassNotFoundException ex){
            ex.printStackTrace();
